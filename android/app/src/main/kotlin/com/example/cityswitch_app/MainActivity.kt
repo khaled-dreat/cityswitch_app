ee840c5f10708534279b3da12585d9e546f9bf41
Khaled Dreat <dreatkhaled@gmail.com>
2025-05-27 22:39:20 +0200

    first comment

---
+++ android/app/src/main/kotlin/com/example/cityswitch_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cityswitch_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:cityswitch_app/app_start/app_start.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
